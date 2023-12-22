302d759bd0743b48db8cb9e276e45d38a96a3e74
Fernando F Ramirez <fernandofabrizzioramirez@gmail.com>
2023-12-22 14:03:02 -0600

    Installing Tailwind
    installing and configuring tailwind.

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -3,11 +3,11 @@ import Header from './components/Header'
 import Formulario from './components/Formulario'
 function App() {
   return (
-    <>
+    <div className='container mx-auto'>
       <Header/>
       <Formulario/>
       <ListadoPacientes/>
-    </>
+    </div>
   )
 }
 
